88d6d34fbb7af40b8d102208ae2c5d4552c9a967
NicksonEdbert <nicksonedbert@gmail.com>
2025-02-15 16:40:16 -0800

    added intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-
-
 export default function Home() {
-  return (
-    <div>
-      <h1>Nickson Wang</h1>
-    </div>
-  );
+    return (
+        <>
+            <section
+                id="intro"
+                className="min-h-screen flex flex-col items-center justify-center bg-gray-100 px-4">
+                <p className="text-xl md:text-2xl text-gray-600 max-w-[750px] text-center">
+                    I'm a developer who loves creating meaningful digital
+                    experiences, with a focus on tech, minimalism, and where
+                    they intersect.
+                </p>
+            </section>
+            <section id="work"></section>
+            <section id="about"></section>
+            <section id="contact"></section>
+        </>
+    );
 }
