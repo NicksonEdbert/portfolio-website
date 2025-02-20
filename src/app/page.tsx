642d010a7f5a6c79ab79e8fbd8a1b2f9ed9ca67b
NicksonEdbert <nicksonedbert@gmail.com>
2025-02-20 10:20:38 -0800

    added simple about section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,8 +51,80 @@ export default function Home() {
                     </div>
                 ))}
             </section>
-            <section id="about"></section>
-            <section id="contact"></section>
+            <section
+                id="about"
+                className="min-h-screen px-4 md:px-24 py-16 md:py-32 md:ml-16">
+                <h2 className="text-4xl md:text-7xl font-medium mb-8 max-w-2xl">
+                    about.
+                </h2>
+                <div className="grid md:grid-cols-3 gap-8">
+                    <div className="md:col-span-1 md:col-start-2">
+                        <div className="space-y-8">
+                            <p className="text-sm text-[#fefeff]">
+                                I&apos;m a fresh graduate currently working as a
+                                manufacturing technician at HP Inc., though my
+                                true passion lies in software engineering and
+                                data science. Based in Seattle, WA, I thrive in an office
+                                environment where I can engage with technology
+                                and innovative ideas. I&apos;ve also had the
+                                opportunity to work with Parchment—a startup in
+                                Austin, TX—which further fueled my drive to grow
+                                in the tech space.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="md:col-span-1 md:col-start-3 md:mt-32">
+                        <div className="space-y-8">
+                            <p className="text-sm text-[#fefeff]">
+                                My approach to problem-solving is methodical: I
+                                break challenges down into smaller tasks and
+                                tackle them steadily and consistently. I believe
+                                in exploring different perspectives and
+                                embracing failures as valuable learning
+                                opportunities. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* <br /> */}
+                <br />
+                <br />
+
+                <div className="grid md:grid-cols-3 gap-8">
+                    <div className="md:col-span-1 md:col-start-2">
+                        <div className="space-y-8">
+                            <p className="text-sm text-[#fefeff]">
+                                In my free time, I pursue a range of activities
+                                that keep both my mind and body active. I enjoy
+                                playing sports, with basketball and golf being
+                                my current focus, and I find creative
+                                satisfaction in personal projects—most notably,
+                                this portfolio website, which I&apos;m incredibly
+                                proud of.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="md:col-span-1 md:col-start-3 md:mt-32">
+                        <div className="space-y-8">
+                            <p className="text-sm text-[#fefeff]">
+                                Looking ahead, my ambition is to build the
+                                capital and expertise necessary to launch my own
+                                business. While I&apos;m still at the beginning of my
+                                professional journey and eager to connect with
+                                mentors and peers, I am committed to continuous
+                                growth and innovation in every aspect of my
+                                work.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section
+                id="contact"
+                className="min-h-screen flex flex-col items-center justify-center px-4"></section>
         </div>
     );
 }
